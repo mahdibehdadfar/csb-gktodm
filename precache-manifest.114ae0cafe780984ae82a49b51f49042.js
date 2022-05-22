1a87b4185164923ac0047d6e4af44ce1f4a5d74b
Code Sandbox <hello@codesandbox.io>
2022-05-22 13:18:37 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.114ae0cafe780984ae82a49b51f49042.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "488fbd1e2e7502e6aa13",
+    "url": "https://mahdibehdadfar.github.io/csb-gktodm/static/css/main.1ff88010.chunk.css"
+  },
+  {
+    "revision": "488fbd1e2e7502e6aa13",
+    "url": "https://mahdibehdadfar.github.io/csb-gktodm/static/js/main.488fbd1e.chunk.js"
+  },
+  {
+    "revision": "a5ef815d2fdeef3094c6",
+    "url": "https://mahdibehdadfar.github.io/csb-gktodm/static/js/1.a5ef815d.chunk.js"
+  },
+  {
+    "revision": "4bc5e0d487cf76dba726",
+    "url": "https://mahdibehdadfar.github.io/csb-gktodm/static/js/runtime~main.4bc5e0d4.js"
+  },
+  {
+    "revision": "c8f1cf3f3d4a01480b3340d594dfa5c9",
+    "url": "https://mahdibehdadfar.github.io/csb-gktodm/index.html"
+  }
+];
